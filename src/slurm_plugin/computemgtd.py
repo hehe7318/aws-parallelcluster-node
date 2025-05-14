f45e1033b9906d19509b2516fb10ef605cf22f73
Xuanqi He <xuanqihe@amazon.com>
2025-05-14 02:24:38 -0400

    Add a 600s sleep before self terminate to test.

--- src/slurm_plugin/computemgtd.py
+++ src/slurm_plugin/computemgtd.py
@@ -144,6 +144,7 @@ def _self_terminate():
         log.info("Using default shutdown command `%s`", shutdown_cmd)
 
     log.info("Self terminating instance now!")
+    time.sleep(600)
     run_command(shutdown_cmd)
 
 
